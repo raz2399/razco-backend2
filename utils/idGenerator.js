375016ef2a5ef9edb9c2b4f5239b943e7a219649
raz2399 <rasmy@razcofoods.net>
2026-07-21 23:57:25 -0700

    Update idGenerator.js

--- utils/idGenerator.js
+++ utils/idGenerator.js
@@ -1,19 +1,9 @@
-const { getDatabase } = require('../config/database');
-
 function generateLoyaltyId() {
-  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-  let id;
-  const db = getDatabase();
-  const check = db.prepare('SELECT id FROM customers WHERE loyalty_id = ?');
-
-  do {
-    let code = '';
-    for (let i = 0; i < 6; i++) {
-      code += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    id = `RZ-${code}`;
-  } while (check.get(id));
-
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let id = 'RZ-';
+  for (let i = 0; i < 8; i++) {
+    id += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
   return id;
 }
 
